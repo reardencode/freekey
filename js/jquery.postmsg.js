d240c87cf33e92a32b663621928f70a983f9116a
Brandon Smith <freedom@reardencode.com>
2011-05-08 13:22:10 -0700

    Working on interframe communication

--- js/jquery.postmsg.js
+++ js/jquery.postmsg.js
@@ -1,5 +1,7 @@
 (function($) {
-    var _state = {bound: false; callbacks: {}, listeners: {}};
+    var _callbacks = {};
+    var _listeners = {};
+    var _bound = false;
 
     var _receive = function(e) {
         try {
@@ -9,106 +11,93 @@
         }
         if (!msg.type)
             return;
-        if (msg.type in _state.callbacks) {
-            _state.callbacks[msg.type](msg.data);
-            delete _state.callbacks[msg.type];
+        if (msg.type in _callbacks) {
+            _callbacks[msg.type](msg.data);
+            delete _callbacks[msg.type];
             return;
         }
-        var funcs = _state.listeners[msg.type] || [];
+        var funcs = _listeners[msg.type] || [];
         for (var i=0; i<funcs.length; i++) {
             var f = funcs[i];
             if (e.origin !== f.origin) {
                 if (msg.errcb)
-                    $.postmsg('send', e.source, msg.errcb, e.origin
+                    $.postmsg.send(e.source, e.origin, e.errcb,
                             "postmsg wrong origin: " + f.origin);
                 continue;
             }
             try {
                 var rv = f.call(msg.data);
                 if (msg.okcb)
-                    $.postmsg('send', e.source, e.origin, msg.okcb, rv);
+                    $.postmsg.send(e.source, e.origin, msg.okcb, rv);
             } catch (ex) {
                 if (msg.errcb)
-                    $.postmsg('send', e.source, e.origin, msg.errcb, ex);
+                    $.postmsg.send(e.source, e.origin, msg.errcb, ex);
             }
         }
     };
 
+    var _init = function() {
+        if (_bound) return;
+        console.log("Setting up event listener...");
+        if (window.addEventListener) {
+            window.addEventListener("message", _receive, false);
+        } else if (window.attachEvent) {
+            window.attachEvent("onmessage", _receive);
+        } else {
+            $.error("Failed to bind postmessage listener");
+        }
+        _bound = true;
+    }
+
     var _makecb = function(json, fn) {
         var cb = new Date().getTime().toString(36) + ':';
         cb += sjcl.codec.base64.fromBits(sjcl.hash.sha1.hash(json));
-        _state.callbacks[cb] = fn;
+        _callbacks[cb] = fn;
         return cb;
     };
 
-    var methods = {
-
+    $.postmsg = {
         send: function(target, origin, type, data, callbacks) {
-            if (!origin || origin === '*')
-                throw("Origin is required for security reasons");
+            if (!origin) $.error("Origin is required for security reasons");
+//            if (origin === 'null') origin = '*';
             var msg = {data:data, type:type};
-            var json = JSON.stringify(msg);
+            var base = JSON.stringify(msg);
             callbacks = callbacks || {};
             if (callbacks.success)
-                msg.okcb = _makecb(json, callbacks.success);
+                msg.okcb = _makecb(base, callbacks.success);
             if (callbacks.error)
-                msg.errcb = _makecb(json, callbacks.error);
+                msg.errcb = _makecb(base, callbacks.error);
             if ("postMessage" in target) {
-                target.postMessage(json, origin);
+                target.postMessage(JSON.stringify(msg), origin);
             } else {
-                throw("No postMessage on target");
+                $.error("No postMessage on target");
             }
         },
 
-        bind: function(type, fn, origin) {
-            _state.listeners[type] = _state.listeners[type] || [];
-            _state.listeners[type].push({fn:fn, origin:origin});
+        listen: function(origin, type, fn) {
+            _init();
+            console.log("Adding listener");
+            _listeners[type] = _listeners[type] || [];
+            _listeners[type].push({call:fn, origin:origin});
         },
 
-        unbind: function(type, fn) {
+        cancel: function(type, fn) {
             if (!type) {
-                _state.listeners = {}; // Unbind everything
+                _listeners = {}; // Unbind everything
                 return;
             }
             if (!fn) {
-                delete _state.listeners[type]; // Unbind this type
+                delete _listeners[type]; // Unbind this type
                 return;
             }
-            var funcs = _state.listeners[type];
+            var funcs = _listeners[type];
             if (!funcs) return;
-            var m = _state.listeners[type] = [];
+            var m = _listeners[type] = [];
             for (var i=0; i<funcs.length; i++) {
                 if (funcs[i].fn !== fn) {
                     m.push(fns[i]);
                 }
             }
-        },
-
-        init: function(options) {
-            // are we already listening to message events on this w?
-            if (_state.bound) return;
-            if (window.addEventListener) {
-                window.addEventListener("message", _dispatch, false);
-            } else if (window.attachEvent) {
-                window.attachEvent("onmessage", _dispatch);
-            } else {
-                throw("Failed to bind postmessage listener");
-            }
-            _state.bound = true;
         }
     };
-
-    $.fn.postmsg = function( method ) {
-        // Method calling logic
-        if (methods[method]) {
-            return methods[method].apply(
-                    this, Array.prototype.slice.call(arguments, 1));
-        } else if (typeof method === 'object' || !method) {
-            return methods.init.apply(this, method);
-        } else {
-            $.error('Method ' +  method + ' does not exist on postmsg');
-        }    
-    };
-
-
 })(jQuery);
